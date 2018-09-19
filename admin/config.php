659b732ef7d168fbd839660d6e44439279e5bd22
root <root@ip-172-31-22-228.us-east-2.compute.internal>
2018-09-19 16:12:57 +0000

    first commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://18.220.163.193/repuestore.com/admin/');
+define('HTTP_CATALOG', 'http://18.220.163.193/repuestore.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://18.220.163.193/repuestore.com/admin/');
+define('HTTPS_CATALOG', 'http://18.220.163.193/repuestore.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/repuestore.com/admin/');
+define('DIR_SYSTEM', '/var/www/html/repuestore.com/system/');
+define('DIR_IMAGE', '/var/www/html/repuestore.com/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/var/www/html/repuestore.com/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'repuestoreuser');
+define('DB_PASSWORD', 'repuestorepass2754');
+define('DB_DATABASE', 'repuestore-db');
+define('DB_PORT', '10000');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
